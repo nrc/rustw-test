43b6dc09c4651fe5d9369e3b2ec64c553b28ffd3
Nick Cameron <ncameron@mozilla.com>
2017-05-15 14:44:25 +1200

    Changes

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
-export RUSTC="/home/ncameron/rust2/x86_64-unknown-linux-gnu/stage2/bin/rustc"
+#export RUSTC="/home/ncameron/rust2/x86_64-unknown-linux-gnu/stage2/bin/rustc"
 cd ..
-./build_templates.sh
 ./cp_static.sh
-cargo build
+#cargo build
 cd test
 #cargo clean
-RUST_LOG=info
+#export RUST_LOG=info
+#export RUST_BACKTRACE=1
 ../target/debug/rustw
+#../target/release/rustw
 
 #../target/debug/rustw -f foo
 #../target/debug/rustw -g "{ \"file_name\": \"src/main.rs\", \"line_start\": 24, \"column_start\": 22, \"line_end\": 24, \"column_end\": 25 }"
